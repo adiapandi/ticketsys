5292238b1623a323e6a1de13c42032e0b99255b5
adi apandi <35246655+adiapandi@users.noreply.github.com>
2026-09-08 16:18:16 +0700

    Add UpdatePreferencesDto for notification settings

---
+++ backend/src/notifications/dto/update-preferences.dto.ts
@@ -0,0 +1,31 @@
+import { IsOptional, IsBoolean } from 'class-validator';
+
+export class UpdatePreferencesDto {
+  @IsOptional()
+  @IsBoolean()
+  emailEnabled?: boolean;
+
+  @IsOptional()
+  @IsBoolean()
+  soundEnabled?: boolean;
+
+  @IsOptional()
+  @IsBoolean()
+  notifyTicketCreated?: boolean;
+
+  @IsOptional()
+  @IsBoolean()
+  notifyTicketAssigned?: boolean;
+
+  @IsOptional()
+  @IsBoolean()
+  notifyStatusChanged?: boolean;
+
+  @IsOptional()
+  @IsBoolean()
+  notifyNewComment?: boolean;
+
+  @IsOptional()
+  @IsBoolean()
+  notifySlaBreached?: boolean;
+}
